59753fdb53bd09981ec411faf3e5e8a2c93f4c12
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-10 23:50:19 -0700

    ear that dog

--- public/client.js
+++ public/client.js
@@ -3,56 +3,63 @@
 
 console.log('hello world :o');
 
-let dreams = [];
+let dogears = [];
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
+const dogearsList = document.getElementById('dogears');
+const createForm = document.forms[0];
+  const createPrefixInput = createForm.elements['prefix'];
+  const createCurrentInput = createForm.elements['current'];
+const updateForm = document.forms[1];
+  const updateCurrentInput = updateForm.elements['current'];
 
-// a helper function to call when our request for dreams is done
-const getDreamsListener = function() {
+// a helper function to call when our request for dogears is done
+const getDogearsListener = function() {
   // parse our response to convert to JSON
-  dreams = JSON.parse(this.responseText);
+  dogears = JSON.parse(this.responseText);
 
-  // iterate through every dream and add it to our page
-  dreams.forEach( function(row) {
-    appendNewDream(row.dream);
+  // Replace the whole dogears list, fuck it
+  dogearsList.innerHTML = '';
+
+  // iterate through every dogear and add it to our page
+  dogears.forEach( function(row) {
+    const newLI = document.createElement('li');
+    newLI.innerHTML = `<a href=${row.current}>${row.current}</a>`
+    dogearsList.appendChild(newLI);
   });
 }
 
-// request the dreams from our app's sqlite database
-const dreamRequest = new XMLHttpRequest();
-dreamRequest.onload = getDreamsListener;
-dreamRequest.open('get', '/getDreams');
-dreamRequest.send();
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
+const refreshDogears = function() {
+  // request the dogears from our app's sqlite database
+  const dogearRequest = new XMLHttpRequest();
+  dogearRequest.onload = getDogearsListener;
+  dogearRequest.open('get', '/list');
+  dogearRequest.send();
 }
 
-// helper to submit
-const submitDream = function(dream) {
-  let dreamPost = new XMLHttpRequest();
-  dreamPost.open('post', '/writeDream');
-  dreamPost.setRequestHeader("Content-Type", "application/json");
-  dreamPost.send(JSON.stringify({"dream": dream}));
+refreshDogears();
+
+// semi-generic helper for submitting an object
+const submitDogear = function(dest, dogObj) {
+  let dogPost = new XMLHttpRequest();
+  dogPost.open('post', dest);
+  dogPost.setRequestHeader("Content-Type", "application/json");
+  dogPost.send(JSON.stringify(dogObj));
 }
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
+// create form:
+createForm.onsubmit = function(e) {
+  e.preventDefault();
+  submitDogear('/create', {prefix: createPrefixInput, current: createCurrentInput});
+  createPrefixInput.value = '';
+  createCurrentInput.value = '';
+  refreshDogears();
+};
 
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  submitDream(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
+// update form:
+updateForm.onsubmit = function(e) {
+  e.preventDefault();
+  submitDogear('/update', {current: updateCurrentInput});
+  updateCurrentInput.value = '';
+  refreshDogears();
 };
