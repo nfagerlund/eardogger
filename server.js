50f678b5c2d96b2071afcb89e4727b80b18b464e
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-15 15:07:08 -0700

    change some vars to consts

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
