0bd3737e003a984496d0e924903972c9b221ffaa
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-16 08:58:39 -0700

    Accept either https? or https?:// for prefix, and start sending the one with slashes

--- server.js
+++ server.js
@@ -68,8 +68,9 @@ app.post('/update', function(req, res){
     db.query(
       "UPDATE dogears " +
         "SET current = $1, current_protocol = $2 WHERE " +
+        "$1 LIKE $2 || prefix || '%' OR " +
         "$1 LIKE $2 || '://' || prefix || '%'",
-      [req.body.current, req.body.current.match(/^https?/)[0]],
+      [req.body.current, req.body.current.match(/^https?:\/\//)[0]],
       function(err){
         if (err) {
           console.log(err);
@@ -95,8 +96,9 @@ app.post('/create', function(req, res){
   db.query("INSERT INTO dogears (prefix, current, current_protocol) VALUES ($1, $2, $3) " +
       "ON CONFLICT (prefix) DO UPDATE " +
       "SET current = $2, current_protocol = $3 WHERE " +
+      "$2 LIKE $3 || EXCLUDED.prefix || '%' OR " +
       "$2 LIKE $3 || '://' || EXCLUDED.prefix || '%'",
-    [prefix, current, current.match(/^https?/)[0]],
+    [prefix, current, current.match(/^https?:\/\//)[0]],
     (err, rows)=>{
       if (err) {
         console.log(err)
