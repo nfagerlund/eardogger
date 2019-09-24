aa830e91e29c5dbc3cc2374dde2ca8696fa755cc
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-24 09:14:43 -0700

    minor client tweaks, make it safari 10.3 safe

--- public/client.js
+++ public/client.js
@@ -15,64 +15,57 @@ const updateForm = document.forms[1];
   const updateCurrentInput = updateForm.elements['current'];
 
 // Get the list of bookmarks from the API, and refresh the on-page list with current info.
-const refreshDogears = async () => {
-  try {
-    const response = await fetch('/api/v1/list', {
-      method: 'GET',
-      credentials: 'include',
-      headers:{'Content-Type': 'application/json', 'Accept': 'application/json'}
+const refreshDogears = () => {
+  fetch('/api/v1/list', {
+    method: 'GET',
+    credentials: 'include',
+    headers:{'Content-Type': 'application/json', 'Accept': 'application/json'}
+  }).then(response => {
+    response.json().then(dogears => {
+      bookmarksList.innerHTML = dogears
+        .map(mark => `<li><a href=${mark.current}>${mark.display_name || mark.current}</a></li>`)
+        .join(' ');
     });
-    const bookmarks = await response.json();
-    // Replace the whole bookmarks list
-    bookmarksList.innerHTML = bookmarks
-      .map( mark => `<li><a href=${mark.current}>${mark.display_name || mark.current}</a></li>` )
-      .join(' ');
-  } catch (e) {
-    bookmarksList.innerHTML = `<li>Something went wrong! Error: ${e}</li>`;
-  }
+  }).catch(err => {
+    bookmarksList.innerHTML = `<li>Something went wrong! Error: ${err}</li>`;
+  });
 }
 
 refreshDogears();
 
 // semi-generic helper for submitting an object
-const submitDogear = async (dest, dogObj) => {
-  try {
-    const response = await fetch(dest, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-      body: JSON.stringify(dogObj)
-    });
-
+const submitDogear = (dest, dogObj) => {
+  fetch(dest, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+    body: JSON.stringify(dogObj)
+  }).then(response => {
     refreshDogears();
-
     return true;
-  } catch (e) {
-    // Later: give feedback that it failed?
+  }).catch(err => {
     return false;
-  }
+  });
 }
 
 // create form:
-createForm.onsubmit = async function(e) {
+createForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  const success = await submitDogear('/api/v1/create', {
+  submitDogear('/api/v1/create', {
     prefix: createPrefixInput.value,
     current: createCurrentInput.value,
     display_name: createNameInput.value
-  });
-  if (success) {
+  }).then(success => {
     createPrefixInput.value = '';
     createCurrentInput.value = '';
     createNameInput.value = '';
-  }
-};
+  });
+});
 
 // update form:
-updateForm.onsubmit = async function(e) {
+updateForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  const success = await submitDogear('/api/v1/update', {current: updateCurrentInput.value});
-  if (success) {
+  submitDogear('/api/v1/update', {current: updateCurrentInput.value}).then(success => {
     updateCurrentInput.value = '';
-  }
-};
+  });
+});
