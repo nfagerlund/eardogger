61dc4a60e54bc05304b7689ffaee9ebd1c204032
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-25 11:08:52 -0700

    wow oops, infinite loop

--- app.js
+++ app.js
@@ -105,8 +105,8 @@ app.use(passport.session());
 app.use(function(req, res, next){
   if (req.user) {
     res.locals.user = req.user;
-    next();
   }
+  next();
 });
 
 // API routes live in their own little thing.
