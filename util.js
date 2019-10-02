bbbd45caba2be226aff37145e69844084f6f5239
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-10-02 09:49:07 -0700

    Add bookmarkletText munger to util

--- util.js
+++ util.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 
 module.exports = {
   readTextFilePromise,
+  bookmarkletText,
 }
 
 
@@ -15,5 +16,30 @@ function readTextFilePromise(file) {
       }
     });
   });
+}
+
+
+// This bookmarklet munger is a port of the Daring Fireball one, and it's pretty
+// naïve. For example, it doesn't understand the syntax of comments enough to
+// catch comments at the end of a line of code, so you need to make sure your
+// bookmarklet JS only puts comments on their own lines. Also you need
+// semicolons always. But that's fine; underthink before u overthink.
+const commentedLines = /^\s*\/\/.+\n/gm;
+const blockComments = /^\s*\/\*.+?\*\/\n?/gm;
+// skipping the tabs to spaces thing, just don't be a tab doofus.
+const spaceRuns = / {2,}/gm;
+const leadingSpace = /^\s+/gm;
+const trailingSpaceAndNewlines = /\s*\n/gm;
+
+// This doesn't handle html escaping, just URI escaping.
+async function bookmarkletText(bookmarklet) {
+  let text = await readTextFilePromise(__dirname + '/bookmarklets/' + bookmarklet + '.js');
+  text = text
+    .replace(commentedLines, '')
+    .replace(blockComments, '')
+    .replace(spaceRuns, ' ')
+    .replace(leadingSpace, '')
+    .replace(trailingSpaceAndNewlines, '');
+  return 'javascript:' + encodeURI(text);
 }
 
