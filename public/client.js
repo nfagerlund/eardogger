a9e349d757833e36db1f4a14370502155ef0a8c9
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-11-27 13:14:30 -0800

    Refactor client JS (make disarming delete buttons less squirelly)
    Well, the disarm thing should be the only visible fix. The rest is just
    arranging things in a less confusing way.

--- public/client.js
+++ public/client.js
@@ -11,6 +11,13 @@ function whenever(callback) {
   }
 }
 
+// page state variables, which some functions refer to.
+// These get filled in with DOM elements (or not) once the document is ready.
+let bookmarksList = null;
+let createForm = null;
+let updateForm = null;
+let countdownIndicator = null;
+
 // clipboard button handler
 function clipboardHandler(button) {
   let target = document.getElementById( button.getAttribute('data-copy-target') );
@@ -58,104 +65,90 @@ function resetButtonStatuses(newStatus, activeButton, allButtons) {
   }
 }
 
+// Build a dogear list item, returning a DOM element. No side-effects.
+function makeDogear(mark) {
+  const li = document.createElement('li');
+
+  const a = document.createElement('a');
+  a.setAttribute('href', mark.current);
+  a.innerText = mark.display_name || mark.prefix;
+  const current = document.createElement('span');
+  current.classList.add('current');
+  current.innerText = '(' + mark.current + ')';
+  const lastDate = document.createElement('span');
+  lastDate.classList.add('date');
+  lastDate.innerText = 'Last read: ' + (new Date(mark.updated)).toLocaleDateString();
+  const destroy = document.createElement('button');
+  destroy.setAttribute('type', 'button')
+  destroy.setAttribute('data-dogear-id', mark.id);
+  destroy.classList.add('delete-dogear');
+  destroy.innerText = 'Delete';
+
+  li.append(a, ' ', current, ' ', lastDate, ' ', destroy);
+
+  return li;
+}
+
+// Get the list of bookmarks from the API, and refresh the on-page list with current info.
+function refreshDogears() {
+  if (!bookmarksList) {
+    return;
+  }
+
+  fetch('/api/v1/list', {
+    method: 'GET',
+    credentials: 'include',
+    headers:{'Content-Type': 'application/json', 'Accept': 'application/json'}
+  }).then(response => {
+    response.json().then(dogears => {
+      bookmarksList.innerHTML = '';
+      bookmarksList.append( ...dogears.map(makeDogear) );
+    });
+  }).catch(err => {
+    bookmarksList.innerHTML = `<li>Something went wrong! Error: ${err}</li>`;
+  });
+}
+
+// Submit a dogear object to the endpoint of your choice. Returns a promise that resolves to a bool (success y/n).
+function submitDogear(dest, dogObj) {
+  return fetch(dest, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+    body: JSON.stringify(dogObj)
+  }).then(response => {
+    if (response.ok) {
+      refreshDogears();
+      return true;
+    } else {
+      return false;
+    }
+  }).catch(_err => {
+    return false;
+  });
+};
+
+// u guessed it,
+function deleteDogear(id) {
+  fetch(`/api/v1/dogear/${id}`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+  }).then(_res => {
+    refreshDogears();
+  });
+}
+
 // OK, go for it.
 whenever(() => {
 
 // some important elements:
-const bookmarksList = document.getElementById('dogears');
-const createForm = document.getElementById('create-dogear');
-const updateForm = document.getElementById('update-dogear');
-const countdownIndicator = document.getElementById('countdown');
+bookmarksList = document.getElementById('dogears');
+createForm = document.getElementById('create-dogear');
+updateForm = document.getElementById('update-dogear');
+countdownIndicator = document.getElementById('countdown');
 
-if (bookmarksList) {
-  // Then we're on the front page and you're logged in.
-
-  // Build a dogear list item, but gotta do it paranoid-like because html injection,
-  // so this returns a DOM element.
-  const makeDogear = mark => {
-    const li = document.createElement('li');
-
-    const a = document.createElement('a');
-    a.setAttribute('href', mark.current);
-    a.innerText = mark.display_name || mark.prefix;
-    const current = document.createElement('span');
-    current.classList.add('current');
-    current.innerText = '(' + mark.current + ')';
-    const lastDate = document.createElement('span');
-    lastDate.classList.add('date');
-    lastDate.innerText = 'Last read: ' + (new Date(mark.updated)).toLocaleDateString();
-    const destroy = document.createElement('button');
-    destroy.setAttribute('type', 'button')
-    destroy.setAttribute('data-dogear-id', mark.id);
-    destroy.classList.add('delete-dogear');
-    destroy.innerText = 'Delete';
-
-    li.append(a, ' ', current, ' ', lastDate, ' ', destroy);
-
-    return li;
-  }
-
-  // Get the list of bookmarks from the API, and refresh the on-page list with current info.
-  const refreshDogears = () => {
-    fetch('/api/v1/list', {
-      method: 'GET',
-      credentials: 'include',
-      headers:{'Content-Type': 'application/json', 'Accept': 'application/json'}
-    }).then(response => {
-      response.json().then(dogears => {
-        bookmarksList.innerHTML = '';
-        bookmarksList.append( ...dogears.map(makeDogear) );
-      });
-    }).catch(err => {
-      bookmarksList.innerHTML = `<li>Something went wrong! Error: ${err}</li>`;
-    });
-  }
-
-  // delegate delete buttons
-  bookmarksList.addEventListener('click', function(e){
-    const that = e.target;
-    if ( that.classList.contains('really-delete-dogear') ) {
-      e.preventDefault();
-      fetch(`/api/v1/dogear/${that.getAttribute('data-dogear-id')}`, {
-        method: 'DELETE',
-        credentials: 'include',
-        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-      }).then(res => {
-        refreshDogears();
-      });
-    } else if ( that.classList.contains('delete-dogear') ) {
-      e.preventDefault();
-      that.classList.add('really-delete-dogear');
-      that.innerText = 'REALLY delete';
-    } else {
-      const reallies = this.getElementsByClassName('really-delete-dogear');
-      // it's a live HTMLCollection, so we have to run the loop backwards. Comedy.
-      for (var i = reallies.length - 1; i >= 0; i--) {
-        reallies[i].innerText = 'Delete';
-        reallies[i].classList.remove('really-delete-dogear');
-      }
-    }
-  });
-
-  // semi-generic helper for submitting a dogear
-  const submitDogear = (dest, dogObj) => {
-    return fetch(dest, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-      body: JSON.stringify(dogObj)
-    }).then(response => {
-      if (response.ok) {
-        refreshDogears();
-        return true;
-      } else {
-        return false;
-      }
-    }).catch(err => {
-      return false;
-    });
-  };
-
+if (updateForm) {
   // Set up manual update form:
   updateForm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -170,7 +163,6 @@ if (bookmarksList) {
     });
   });
 }
-
 
 if (countdownIndicator) {
   // we're redirecting soon.
@@ -188,7 +180,7 @@ if (countdownIndicator) {
 }
 
 // Munge a default prefix for normal version of the create form
-if (createForm && createForm.elements['prefix'].value) {
+if (createForm && createForm.elements['prefix'] && createForm.elements['prefix'].value) {
   const prefix = createForm.elements['prefix'];
   const changePrefix = document.getElementById('change-prefix');
 
@@ -197,9 +189,9 @@ if (createForm && createForm.elements['prefix'].value) {
   prefix.value = prefixHost;
   prefix.readOnly = true;
   prefix.classList.add('read-only');
-  changePrefix.style.display = 'inline-block';
+  changePrefix.style.display = 'inline-block'; // 'cause it's hidden by default.
 
-  changePrefix.addEventListener('click', function(e){
+  changePrefix.addEventListener('click', function(_e){
     this.style.display = 'none';
     prefix.readOnly = false;
     prefix.classList.remove('read-only');
@@ -208,17 +200,36 @@ if (createForm && createForm.elements['prefix'].value) {
   });
 }
 
-// Toggle help when help buttons are clicked
+// The big "clicking on buttons" listener
 document.body.addEventListener('click', function(e){
   const that = e.target;
   if (that.matches('.help-reveal')) {
+    // Help text toggle buttons:
     e.preventDefault();
     const helpTarget = document.getElementById( that.getAttribute('data-help-target') );
     helpTarget.classList.toggle('help-hidden');
     that.classList.toggle('help-reveal-active');
   } else if (that.matches('.copy-button')) {
+    // Clipboard copy buttons:
     e.preventDefault();
     clipboardHandler(that);
+  } else if (that.matches('.really-delete-dogear')) {
+    // Armed delete buttons (order matters, must check this before the next one):
+    e.preventDefault();
+    deleteDogear(that.getAttribute('data-dogear-id'));
+  } else if (that.matches('.delete-dogear')) {
+    // Unarmed delete buttons:
+    e.preventDefault();
+    that.classList.add('really-delete-dogear');
+    that.innerText = 'REALLY delete';
+  } else {
+    // Disarm delete buttons when clicking elsewhere:
+    const reallies = this.getElementsByClassName('really-delete-dogear');
+    // it's a live HTMLCollection, so we have to run the loop backwards. Comedy.
+    for (var i = reallies.length - 1; i >= 0; i--) {
+      reallies[i].innerText = 'Delete';
+      reallies[i].classList.remove('really-delete-dogear');
+    }
   }
 });
 
