ff06a8fd0df3e8b82f32cebf653127297ce9d7f3
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-08-28 14:01:29 -0700

    Migration: add tokens table

---
+++ migrations/sqls/20210828204743-tokens-table-up.sql
@@ -0,0 +1,9 @@
+CREATE TYPE token_scope AS ENUM ('write_dogears', 'manage_dogears');
+
+CREATE TABLE tokens (
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id integer REFERENCES users (id) ON DELETE CASCADE,
+  token_hash text,
+  scope token_scope,
+  comment text,
+);
