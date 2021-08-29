a64b3be4d4e727bf8a35acc833006beb23a51079
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-08-28 22:55:20 -0700

    Convert db/pg to typescript, for good measure

--- db/pg.ts
+++ db/pg.ts
@@ -10,6 +10,6 @@ const pool = new Pool({
 module.exports = {
   // Returns a promise that resolves to a pg.Result, which has .rows, .fields, and .rowCount.
   // https://node-postgres.com/api/result
-  query: (text, params) => pool.query(text, params),
+  query: (text: string, params: Array<any>) => pool.query(text, params),
   pool: pool,
 };
