f42c82d8705138cd4762dc3d9d77af4d68307279
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-08-30 13:29:16 -0700

    Oops! Add created timestamp to tokens table
    I'm just using the same migration because it's never hit prod
    and I can wipe the table in dev.

--- migrations/sqls/20210828204743-tokens-table-up.sql
+++ migrations/sqls/20210828204743-tokens-table-up.sql
@@ -5,5 +5,6 @@ CREATE TABLE tokens (
   user_id integer REFERENCES users (id) ON DELETE CASCADE,
   token_hash text,
   scope token_scope,
+  created timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
   comment text
 );
