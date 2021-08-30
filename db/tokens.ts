bd0c399fb4edbb7ee0c09777014ad5952a121246
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-08-30 13:44:40 -0700

    Update interface and create function to handle creation date

--- db/tokens.ts
+++ db/tokens.ts
@@ -7,12 +7,14 @@ interface Token {
   id: number,
   user_id: number,
   scope: TokenScope,
+  created: Date,
   comment: string,
   token?: string,
 };
 
 module.exports = {
   create,
+  list,
 };
 
 // Create and store a token; must return the cleartext of the token, as it's
@@ -21,15 +23,16 @@ async function create(userID: number, scope: TokenScope, comment: string): Promi
   let tokenCleartext = `eardoggerv1.${uuidv4()}`;
   let tokenHash = crypto.createHash('sha256').update(tokenCleartext).digest('hex');
   let result = await db.query(
-    "INSERT INTO tokens (user_id, token_hash, scope, comment) VALUES ($1, $2, $3, $4) RETURNING id",
+    "INSERT INTO tokens (user_id, token_hash, scope, comment) VALUES ($1, $2, $3, $4) RETURNING id, created",
     [userID, tokenHash, scope, comment]
   );
-  let id = result.rows[0].id;
+  let { id, created } = result.rows[0].id;
   return {
     id,
     token: tokenCleartext,
     user_id: userID,
     scope,
+    created,
     comment,
   };
 }
