8192357ac035ce16bcd96265f843e5ca7978446f
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-09-07 16:30:01 -0700

    server.ts: Fix broken import from app, fix type checking

--- server.ts
+++ server.ts
@@ -1,7 +1,14 @@
 // load fully-configured express app
-let app = require('./app');
+import app from './app';
 
 // listen for requests
 let listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  let address = listener.address();
+  if (typeof address === 'string') {
+    console.log(`Got something unexpected back from Server.address(): ${address}`);
+  } else if (address === null) {
+    console.log('Got null back from Server.address()??');
+  } else {
+    console.log('Your app is listening on port ' + address.port);
+  }
 });
