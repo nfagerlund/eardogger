0c854838497a875f3d3dc49bf0effe42b17dee0b
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-09-08 14:17:34 -0700

    Fairly comprehensive logged-in tests!

--- __tests__/appLoggedIn.ts
+++ __tests__/appLoggedIn.ts
@@ -1,14 +1,20 @@
 import { describe, expect, test, jest } from '@jest/globals';
 import { Request, Response, NextFunction } from 'express';
 import request from 'supertest';
+import type { Response as SuperResponse } from 'supertest';
+import { JSDOM } from 'jsdom';
 import * as mocks from '../db/mocks';
 
 // First, mock the session persistence middleware, which is the only thing that
 // hits the database directly instead of using my DB layer.
 function fakeSession() {
   return function(req: Request, res: Response, next: NextFunction) {
+    let user = parseInt(req.get('X-Test-User-Override') || '');
+    if (isNaN(user)) {
+      user = 1;
+    }
     // @ts-ignore sorry, but
-    req.session = { passport: { user: 1 } }; // cf. user mock data
+    req.session = { passport: { user } }; // cf. user mock data
     next();
   }
 }
@@ -40,9 +46,115 @@ jest.mock('../db/tokens', () => {
 // Okay, finally we can do some tests.
 import app from '../app';
 
-describe("account page", () => {
-  test("it 200s", async () => {
+function makeDoc(text: string) {
+  return (new JSDOM(text)).window.document;
+}
+
+describe("/account", () => {
+  test("it renders", async () => {
     let response = await request(app).get('/account');
     expect(response.statusCode).toBe(200);
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^Manage account/);
+    expect(doc.getElementById('changepasswordform')).toBeTruthy();
   });
+});
+
+describe("index page", () => {
+  test("it renders", async () => {
+    let response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/Dogears —/);
+    expect(doc.getElementById('dogears')).toBeTruthy();
+    expect(doc.getElementById('update-dogear')).toBeTruthy();
+    expect(doc.querySelectorAll('#dogears > li').length > 0).toBe(true);
+    // structure of a dogear LI:
+    expect(doc.querySelector('#dogears > li > a')).toBeTruthy();
+    expect(doc.querySelector('#dogears > li > .current')).toBeTruthy();
+    expect(doc.querySelector('#dogears > li > .date')).toBeTruthy();
+    expect(doc.querySelector('#dogears > li > .delete-dogear')).toBeTruthy();
+  });
+});
+
+describe("faq page", () => {
+  test("it renders", async () => {
+    let response = await request(app).get('/faq');
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^How to/);
+    expect(doc.getElementById('what-is')).toBeTruthy();
+    expect(doc.getElementById('faq')).toBeTruthy();
+  });
+});
+
+describe("install page", () => {
+  test("it renders", async () => {
+    let response = await request(app).get('/install');
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^Install/);
+    expect(doc.getElementById('install')).toBeTruthy();
+  });
+});
+
+describe("/fragments/dogears", () => {
+  test("it renders", async () => {
+    let response = await request(app).get('/fragments/dogears');
+    expect(response.statusCode).toBe(200);
+    let doc = JSDOM.fragment(response.text);
+    expect(doc.getElementById('dogears')).toBeFalsy(); // we only get the innerHTML of the UL
+    expect(doc.getElementById('update-dogear')).toBeFalsy(); // not an index page
+    expect(doc.querySelectorAll('li').length > 0).toBe(true);
+    // structure of a dogear LI:
+    expect(doc.querySelector('li > a')).toBeTruthy();
+    expect(doc.querySelector('li > .current')).toBeTruthy();
+    expect(doc.querySelector('li > .date')).toBeTruthy();
+    expect(doc.querySelector('li > .delete-dogear')).toBeTruthy();
+  });
+});
+
+describe("/mark/:url", () => {
+  test("it renders the 'marked' view if update succeeds", async () => {
+    let response = await request(app).get('/mark/https%3A%2F%2Fexample.com%2Fcomic%2F25');
+    expect(response.statusCode).toBe(200);
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^Saved your place/);
+    expect(doc.getElementById('mark-success')).toBeTruthy();
+  });
+
+  test("it renders the 'create' view if update fails", async () => {
+    // Need to override the logged-in user to get a whiff back from the update stub:
+    let response = await request(app).get('/mark/https%3A%2F%2Fexample.com%2Fcomic%2F25')
+      .set('X-Test-User-Override', '2');
+    expect(response.statusCode).toBe(200);
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^Dogear this/);
+    expect(doc.getElementById('create-dogear')).toBeTruthy();
+  });
+});
+
+describe("POST /mark", () => {
+  test("it renders the 'marked' view after creating a new dogear", async () => {
+    let response = await request(app).post('/mark')
+      .type('form')
+      .send({
+        prefix: 'https://example.com/newcomic/',
+        current: 'https://example.com/newcomic/2',
+        display_name: 'New comic',
+      });
+    expect(response.statusCode).toBe(200);
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^Saved your place/);
+    expect(doc.getElementById('mark-success')).toBeTruthy();
+  });
+});
+
+describe("/resume/:url", () => {
+  test("it redirects off-site", async () => {
+    let response = await request(app).get('/resume/https%3A%2F%2Fexample.com%2Fcomic%2F25');
+    expect(response.statusCode).toBe(302);
+    expect(response.header['location']).toMatch(/^https?:/);
+  });
+
+  // Missing a test for going to the "create" view if it whiffs, but I'm less
+  // concerned about that. (Would need to update mocks.)
 });
