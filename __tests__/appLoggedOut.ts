f8fcbc833f31bc8d5d113353bd028f222857e095
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-09-08 13:22:33 -0700

    Comprehensive app tests for logged-out cases!

--- __tests__/appLoggedOut.ts
+++ __tests__/appLoggedOut.ts
@@ -1,13 +1,18 @@
 import { describe, expect, test, jest } from '@jest/globals';
 import { Request, Response, NextFunction } from 'express';
 import request from 'supertest';
+import type { Response as SuperResponse } from 'supertest';
+import { JSDOM } from 'jsdom';
 import * as mocks from '../db/mocks';
 
 // First, mock the session persistence middleware, which is the only thing that
 // hits the database directly instead of using my DB layer.
 function fakeSession() {
   return function(req: Request, res: Response, next: NextFunction) {
-    // For logged-out, we want no session info.
+    // For logged-out, we want no session info... but need a session object to
+    // prevent 500s.
+    // @ts-ignore sorry, but
+    req.session = {};
     next();
   }
 }
@@ -45,3 +50,65 @@ describe("account page", () => {
     expect(response.statusCode).toBe(401);
   });
 });
+
+function makeDoc(text: string) {
+  return (new JSDOM(text)).window.document;
+}
+
+function expectLoginPage(response: SuperResponse) {
+  expect(response.statusCode).toBe(200);
+  let doc = makeDoc(response.text);
+  expect(doc.getElementById('signupform')).toBeTruthy();
+  // Oof, need an ID on that login form
+  expect(doc.querySelector('form[action="/login"]')).toBeTruthy();
+  expect(doc.getElementById('dogears')).toBeFalsy(); // index.hbs
+  expect(doc.getElementById('update-dogear')).toBeFalsy(); // index.hbs
+  expect(doc.getElementById('mark-success')).toBeFalsy(); // marked.hbs
+  expect(doc.getElementById('create-dogear')).toBeFalsy(); // create.hbs
+}
+
+describe("pages that are actually the login page", () => {
+  test("index page", async () => {
+    let response = await request(app).get('/');
+    expectLoginPage(response);
+  });
+
+  test("/mark/:url", async () => {
+    let response = await request(app).get('/mark/https%3A%2F%2Fexample.com%2Fcomic%2F25');
+    expectLoginPage(response);
+  });
+
+  test("/resume/:url", async () => {
+    let response = await request(app).get('/resume/https%3A%2F%2Fexample.com%2Fcomic%2F25');
+    expectLoginPage(response);
+  });
+});
+
+describe("faq page", () => {
+  test("it renders", async () => {
+    let response = await request(app).get('/faq');
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^How to/);
+    expect(doc.getElementById('what-is')).toBeTruthy();
+    expect(doc.getElementById('faq')).toBeTruthy();
+  });
+});
+
+describe("install page", () => {
+  test("it renders", async () => {
+    let response = await request(app).get('/install');
+    let doc = makeDoc(response.text);
+    expect(doc.title).toMatch(/^Install/);
+    expect(doc.getElementById('install')).toBeTruthy();
+  });
+});
+
+describe("/fragments/dogears", () => {
+  test("it 404s", async () => {
+    let response = await request(app).get('/fragments/dogears');
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+// The one missing test is POST /mark (redirects to index), but I'm not too
+// concerned about that one.
