c283d4d3088b115daeec21a9338a21d455ca05fc
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-09-08 12:17:03 -0700

    Add basic test for logged-out account page
    At this point, we have all the skeletons and the main remaining task
    is filling in the application tests.

---
+++ __tests__/appLoggedOut.ts
@@ -0,0 +1,47 @@
+import { describe, expect, test, jest } from '@jest/globals';
+import { Request, Response, NextFunction } from 'express';
+import request from 'supertest';
+import * as mocks from '../db/mocks';
+
+// First, mock the session persistence middleware, which is the only thing that
+// hits the database directly instead of using my DB layer.
+function fakeSession() {
+  return function(req: Request, res: Response, next: NextFunction) {
+    // For logged-out, we want no session info.
+    next();
+  }
+}
+jest.mock('../session');
+import initializeSession from '../session';
+// @ts-ignore
+initializeSession.mockImplementation(fakeSession);
+
+// Then mock the entire DB layer
+jest.mock('../db/dogears', () => {
+  return {
+    __esModule: true,
+    ...mocks.dogears,
+  };
+});
+jest.mock('../db/users', () => {
+  return {
+    __esModule: true,
+    ...mocks.users,
+  };
+});
+jest.mock('../db/tokens', () => {
+  return {
+    __esModule: true,
+    ...mocks.tokens,
+  };
+});
+
+// Okay, finally we can do some tests.
+import app from '../app';
+
+describe("account page", () => {
+  test("it 401s", async () => {
+    let response = await request(app).get('/account');
+    expect(response.statusCode).toBe(401);
+  });
+});
